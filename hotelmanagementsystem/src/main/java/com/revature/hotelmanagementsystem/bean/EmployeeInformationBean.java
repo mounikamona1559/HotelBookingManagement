60183ed1b8b6ff0d4f4981ef4db69aedd98bab5e
RGUKT <RGUKT@DESKTOP-IM5MQMU>
2022-06-16 11:06:46 +0530

    hotel

--- hotelmanagementsystem/src/main/java/com/revature/hotelmanagementsystem/bean/EmployeeInformationBean.java
+++ hotelmanagementsystem/src/main/java/com/revature/hotelmanagementsystem/bean/EmployeeInformationBean.java
@@ -4,7 +4,7 @@ import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.Id;
 import javax.persistence.Table;
-
+//
 @Entity
 @Table(name = "employee_information")
 public class EmployeeInformationBean {
